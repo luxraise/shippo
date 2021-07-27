618667f1cb4fa46270fe0b47f29925c0633f5e92
BrandenWilliams <32830332+BrandenWilliams@users.noreply.github.com>
2021-07-27 15:14:32 -0700

    add form support for AddressTo

--- shipment.go
+++ shipment.go
@@ -2,6 +2,6 @@ package shippo
 
 type Shipment struct {
 	AddressFrom Address  `json:"address_from"`
-	AddressTo   Address  `json:"address_to"`
+	AddressTo   Address  `json:"address_to" form:"parcels"`
 	Parcels     []Parcel `json:"parcels"`
 }
