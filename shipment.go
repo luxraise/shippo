3224f4898c21263f07c0c25d4827f0c647c4cfc9
BrandenWilliams <32830332+BrandenWilliams@users.noreply.github.com>
2021-07-27 15:42:25 -0700

    Update shipment.go

--- shipment.go
+++ shipment.go
@@ -1,7 +1,7 @@
 package shippo
 
 type Shipment struct {
-	AddressFrom Address  `json:"address_from"`
+	AddressFrom Address  `json:"address_from" form:"address_form"`
 	AddressTo   Address  `json:"address_to" form:"address_to"`
-	Parcels     []Parcel `json:"parcels"`
+	Parcels     []Parcel `json:"parcels" form:"parcels"`
 }
