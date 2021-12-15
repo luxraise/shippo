0c6fdff5219f89e20597905e36780535a75a6547
Josh Montoya <josh@montoya.io>
2021-12-15 15:41:18 -0800

    implement MakeShipment

--- shipment.go
+++ shipment.go
@@ -1,5 +1,13 @@
 package shippo
 
+// MakeShipment is a quality of life helper function to make a new instance of Shipment
+func MakeShipment(from, to Address, parcels ...Parcel) (s Shipment) {
+	s.AddressFrom = from
+	s.AddressTo = to
+	s.Parcels = parcels
+	return
+}
+
 type Shipment struct {
 	AddressFrom Address  `json:"address_from" form:"address_form"`
 	AddressTo   Address  `json:"address_to" form:"address_to"`
